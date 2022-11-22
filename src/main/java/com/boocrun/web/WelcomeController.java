79e4ecd0d9d5b059723c6b84937b9f1f6873233d
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-11-21 22:27:40 -0500

    Added ModelMap

--- src/main/java/com/boocrun/web/WelcomeController.java
+++ src/main/java/com/boocrun/web/WelcomeController.java
@@ -1,14 +1,16 @@
 package com.boocrun.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController 
 {
 	@RequestMapping("/")		
-	public String welcome () 
+	public String welcome (ModelMap model) 
 	{
+		model.put("message", "This message comes to yu from our Welcome Controller");
 		return "welcome";
 		
 	}
