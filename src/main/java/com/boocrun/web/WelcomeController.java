d19260eafa934784d11a8abce61139d99b7a34c1
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-11-23 11:28:29 -0500

    Added form with Thymeleaf

--- src/main/java/com/boocrun/web/WelcomeController.java
+++ src/main/java/com/boocrun/web/WelcomeController.java
@@ -1,17 +1,47 @@
 package com.boocrun.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.boocrun.domain.Car;
 
 @Controller
 public class WelcomeController 
 {
-	@RequestMapping("/")		
+	@RequestMapping(value="/", method=RequestMethod.GET)		
 	public String welcome (ModelMap model) 
 	{
-		model.put("message", "This message comes to yu from our Welcome Controller");
+		populateModel(model);
+		
 		return "welcome";
 		
+	}
+
+	private void populateModel(ModelMap model) {
+		model.put("message", "This message comes to yu from our Welcome Controller");
+		List<String> cars = new ArrayList<>();
+		cars.add("Toyota");
+		cars.add("Honda");
+		cars.add("Tesla");
+		cars.add("Ford");
+		cars.add("GM");
+		model.put("cars", cars);
+		
+		Car car = new Car();
+		model.put("car", car); 
+		
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String welcomPast (@ModelAttribute Car car, ModelMap model) {
+		populateModel(model);
+		System.out.println(car);
+		return "welcome";
 	}
 }
