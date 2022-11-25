07eb208f94c44ce5995af99188aaba868240e94a
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-11-24 23:32:08 -0500

    delete  from list wjth thymeleaf

--- src/main/java/com/boocrun/web/WelcomeController.java
+++ src/main/java/com/boocrun/web/WelcomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,10 +14,25 @@ import com.boocrun.domain.Car;
 
 @Controller
 public class WelcomeController 
-{
+{	
+	List<String> cars = new ArrayList<>();
+	
+	public WelcomeController () {
+		cars.add("Toyota");
+		cars.add("Honda");
+		cars.add("Tesla");
+		cars.add("Ford");
+		cars.add("GM");
+		cars.add("BMW");
+		cars.add("Mazda");
+		cars.add("Chevrolet");
+		
+	}
+	
 	@RequestMapping(value="/", method=RequestMethod.GET)		
 	public String welcome (ModelMap model) 
 	{
+		
 		populateModel(model);
 		
 		return "welcome";
@@ -25,12 +41,8 @@ public class WelcomeController
 
 	private void populateModel(ModelMap model) {
 		model.put("message", "This message comes to yu from our Welcome Controller");
-		List<String> cars = new ArrayList<>();
-		cars.add("Toyota");
-		cars.add("Honda");
-		cars.add("Tesla");
-		cars.add("Ford");
-		cars.add("GM");
+
+
 		model.put("cars", cars);
 		
 		Car car = new Car();
@@ -42,6 +54,13 @@ public class WelcomeController
 	public String welcomPast (@ModelAttribute Car car, ModelMap model) {
 		populateModel(model);
 		System.out.println(car);
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/del/{carBrand}", method=RequestMethod.GET)
+	public String deleteCarBrend (@PathVariable String carBrand, ModelMap model) {
+		cars.remove(carBrand);
+		populateModel(model);
 		return "welcome";
 	}
 }
