5bef058bbaed82422f07f222f26859443e02e2e8
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-11-30 21:07:44 -0500

    wrong object like url

--- src/main/java/com/boocrun/domain/Car.java
+++ src/main/java/com/boocrun/domain/Car.java
@@ -1,33 +1,66 @@
 package com.boocrun.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Car {
 	private String brand;
-	private Integer year;
-	private String fuelType;
+	private List <Model> models = new ArrayList<>();
 	
-	
+	public List<Model> getModels() {
+		return models;
+	}
+	public void setModels(List<Model> models) {
+		this.models = models;
+	}
 	public String getBrand() {
 		return brand;
 	}
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
-	public Integer getYear() {
-		return year;
+/*	public Integer getYear() {
+		return data.getYear();
 	}
 	public void setYear(Integer year) {
-		this.year = year;
+		this.data.setYear(year);
 	}
 	public String getFuelType() {
-		return fuelType;
+		return data.getFuelType();
 	}
 	public void setFuelType(String fuelType) {
-		this.fuelType = fuelType;
-	}
+		this.data.setFuelType(fuelType);
+	}*/
 	@Override
 	public String toString() {
-		return "Car [brand=" + brand + ", year=" + year + ", fuelType=" + fuelType + "]";
+		return "Car [brand=" + brand + ", models=" + models.toArray() + "]";
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((brand==null) ? 0 : brand.hashCode());
+		return result;
+	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Car other = (Car) obj;
+		if (brand == null)
+		{
+			if (other.brand != null)
+				return false;
+		}
+		else if (!brand.equals(other.brand))
+			return false;
+		return true;
+	}
 }
---
+++ src/main/java/com/boocrun/domain/Model.java
@@ -0,0 +1,36 @@
+package com.boocrun.domain;
+
+public class Model {
+	private String name;
+	private Integer year;
+	private String fuelType;
+
+	public Model() {
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
